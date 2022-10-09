77735ca065c204936de79a24b38a9daed59cc214
artem pecheny <pecheny@gmail.com>
2022-10-09 15:07:00 +0300

    Fix axis iterator type.

--- src/macros/BuildMacro.hx
+++ src/macros/BuildMacro.hx
@@ -4,12 +4,25 @@ import haxe.macro.Expr.ComplexType;
 import haxe.macro.Expr.Access;
 import haxe.ds.ReadOnlyArray;
 import haxe.macro.Context;
+
 using haxe.macro.TypeTools;
+using StringTools;
 
 class BuildMacro {
 	public static function buildAxes() {
-		var t= Context.getLocalType();
-		var ct = t.toComplexType();
+		var t = Context.getLocalType();
+		var ct:ComplexType = t.toComplexType();
+		var baseCt:ComplexType = switch ct {
+			case TPath(tp):
+				TPath({
+					name: tp.name.replace("_Impl_", ""),
+					params: tp.params,
+					pack: tp.pack,
+					sub: tp.sub
+				});
+			case _: throw "Wrong";
+		}
+
 		var fields = Context.getBuildFields();
 		var vals = [];
 		for (f in fields) {
@@ -39,7 +52,7 @@ class BuildMacro {
 			kind: FieldType.FFun({
 				args: [],
 				expr: macro return cast $p{["Axis", "k" + vals.length, "iterator"]}(),
-				ret: macro:Iterator<$ct>
+				ret: macro:Iterator<$baseCt>
 			}),
 			access: [APublic, AStatic]
 		});
@@ -73,8 +86,6 @@ class BuildMacro {
 		// 	}),
 		// 	access: [APublic, AInline]
 		// });
-
-
 		// fields.push(macro final aliases = $a{[macro "foo"]});
 		return fields;
 	}
@@ -85,13 +96,13 @@ class BuildMacro {
 			case TAbstract(_.get() => ab, params) if (ab.meta.has(":enum")):
 				for (field in ab.impl.get().statics.get()) {
 					if (field.meta.has(":enum") && field.meta.has(":impl")) {
-					  var fieldName = field.name;
-					  num++;
-					//   valueExprs.push(macro $typePath.$fieldName);
+						var fieldName = field.name;
+						num++;
+						//   valueExprs.push(macro $typePath.$fieldName);
 					}
-				  }
+				}
 
-			case _:
+			case _:Context.fatalError("The argument should be @:enum abstract. Probably your call from @:generic class where you should set number of axes explicitly", Context.currentPos());
 		}
 		return num;
 	}
