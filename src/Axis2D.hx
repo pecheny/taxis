a7883695385995784f44851c80cd5e1343196c06
artem pecheny <pecheny@gmail.com>
2022-11-22 21:36:14 +0300

    Wipe collection alias remaining.

--- src/Axis2D.hx
+++ src/Axis2D.hx
@@ -5,6 +5,4 @@
 }
 
 typedef AVector2D<T> = AVector<Axis2D, T>;
-typedef AxisCollection2D<T> = AVector<Axis2D, T>;
 typedef ReadOnlyAVector2D<T> = AVector.ReadOnlyAVector<Axis2D, T>;
-typedef ROAxisCollection2D<T> = ReadOnlyAVector2D<T>;
