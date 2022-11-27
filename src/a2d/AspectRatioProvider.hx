d7b94f29bfd9cb63537af513220b996dfa1d47ed
artem pecheny <pecheny@gmail.com>
2022-11-27 10:07:24 +0300

    getAspectRatio() naming.

--- src/a2d/AspectRatioProvider.hx
+++ src/a2d/AspectRatioProvider.hx
@@ -1,6 +1,5 @@
 package a2d;
-import Axis2D.ReadOnlyAVector2D;
 interface AspectRatioProvider {
-    public function getFactorsRef():AspectRatio;
+    public function getAspectRatio():AspectRatio;
 }
 
