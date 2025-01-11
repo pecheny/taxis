1b418c0df86e0a7437f69196fa1ef167a8566fa0
artem pecheny <pecheny@gmail.com>
2025-01-11 05:51:28 +0300

    Axis2D.other().

--- src/Axis2D.hx
+++ src/Axis2D.hx
@@ -2,6 +2,10 @@
 @:enum abstract Axis2D(Axis<Axis2D>) to Axis<Axis2D> to Int {
     var horizontal;
     var vertical;
+
+    public inline function other():Axis2D {
+        return if (this == horizontal) vertical else horizontal;
+    }
 }
 
 typedef AVector2D<T> = AVector<Axis2D, T>;
